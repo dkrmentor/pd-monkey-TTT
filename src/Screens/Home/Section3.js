e08ea17cc439a1da10c9de759fc66d0847ac22c7
dkrmentor <dkrmentor@gmail.com>
2022-11-19 21:59:03 +0500

    pd

--- src/Screens/Home/Section3.js
+++ src/Screens/Home/Section3.js
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import woman from '../../Images/woman.png';
+import Nav from 'react-bootstrap/Nav';
 const Section3 = () => {
   return (
-    <div>
-      
+    <div className='setion3'>
+      <div className="container-fluid">
+        <div className="row">
+          <div className='col-md-2'>
+
+          </div>
+
+          <div className="carousel2 col-md-4 woman-img spacing">
+            <img width={600} src={woman} alt="man" />
+
+          </div>
+          <div className="col-md-4 ">
+            <div className=" trst-txt-div3 spacing">
+              <p>REACH A <span> LARGER AUDIENCE</span></p>
+              <h3>Video Podcast <br />
+                Editing &  <span> Social Clips  </span> </h3>
+              <p>
+                Get YouTube ready episodes, and short clips to promote on all your social media. We'll turn your riverside.fm or zoom recordings into clean video episodes.
+              </p>
+
+            </div>
+
+          </div>
+          <div className='col-md-1'>
+
+          </div>
+        </div>
+      </div>
+
+      <div className='sec-card3'>
+
+</div>
     </div>
   )
 }
